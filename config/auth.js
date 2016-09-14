2c7ccaa17edab45ff478ea05c472a8e478c05453
Christopher Ong <chrisongts@gmail.com>
2016-09-14 13:50:08 +0800

    changed auth.js

--- config/auth.js
+++ config/auth.js
@@ -2,12 +2,14 @@ module.exports = {
   facebookAuth: {
     clientID: '516578915211993',
     clientSecret: '62665d4e360ca7644543449fdfb2631f',
-    callbackURL: 'http://localhost:3000/auth/facebook/callback',
+    // callbackURL: 'http://localhost:3000/auth/facebook/callback',
+    callbackURL: 'https://evening-river-56589.herokuapp.com/auth/facebook/callback',
   },
   twitterAuth: {
     consumerKey: 'MKc6DUO9lfUnc4XIubutW2mn9',
     consumerSecret: 'v25kVyoT7ohDoQniYYJpIpZ3N8XCvndaL56QtCLgBL1QZQ53F8',
-    callbackURL: 'http://localhost:3000/auth/twitter/callback',
+    // callbackURL: 'http://localhost:3000/auth/twitter/callback',
+    callbackURL: 'https://evening-river-56589.herokuapp.com/auth/twitter/callback',
   },
   // googleAuth: {
   //   clientID: 'YOUR-GOOGLE-CLIENT-ID',
